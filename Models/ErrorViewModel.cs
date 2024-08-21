10e77eef8d4d1f31ded00078ead7a9a38abbef9e
yazilimciabi1 <ugur.calim.93@gmail.com>
2024-08-21 22:30:11 +0300

    Project files created.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrailleNet.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
