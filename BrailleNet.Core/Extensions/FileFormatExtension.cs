333e684006687937eff774a8ee09296ae72e0bc8
yazilimciabi1 <ugur.calim.93@gmail.com>
2024-08-21 23:57:31 +0300

    FileFormat & Service registration extension methods developed.

---
+++ BrailleNet.Core/Extensions/FileFormatExtension.cs
@@ -0,0 +1,30 @@
+﻿using BrailleNet.Core.Types;
+
+namespace BrailleNet.Core.Extensions
+{
+    public static class FileFormatExtension
+    {
+        public static string ToExtensionString(this FileFormat fileFormat)
+        {
+            switch (fileFormat)
+            {
+                case FileFormat.Txt:
+                    return ".txt";
+                case FileFormat.Brf:
+                    return ".brf";
+                case FileFormat.Brl:
+                    return ".brl";
+                case FileFormat.Pef:
+                    return ".pef";
+                case FileFormat.Dxb:
+                    return ".dxb";
+                case FileFormat.Abt:
+                    return ".abt";
+                case FileFormat.Utb:
+                    return ".utb";
+                default:
+                    return ".undefined";
+            }
+        }
+    }
+}
---
+++ BrailleNet.Core/Extensions/ServiceExtension.cs
@@ -0,0 +1,18 @@
+﻿using BrailleNet.Core.Implementations.Importers;
+using BrailleNet.Core.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using BrailleNet.Core.Implementations.Translators;
+using BrailleNet.Core.Implementations.Converters;
+
+namespace BrailleNet.Core.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void RegisterBrailleNetServices(this IServiceCollection services)
+        {
+            services.AddScoped<ILanguageImporter, LanguageImporter>();
+            services.AddScoped<ITextConverter, TextToBrailleConverter>();
+            services.AddScoped<ITextTranslator, TextTransator>();
+        }
+    }
+}
